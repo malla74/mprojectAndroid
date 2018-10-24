7e4aa4f62983507a8670f2bb54eb9f92f01d8a21
malla74 <43986742+malla74@users.noreply.github.com>
2018-10-24 14:27:21 +0530

    Update TC47_SignInFromRewardsProgramInfoTest.java

--- test/TestNG/src/test/java/com/michaels/mobile/test/TC47_SignInFromRewardsProgramInfoTest.java
+++ test/TestNG/src/test/java/com/michaels/mobile/test/TC47_SignInFromRewardsProgramInfoTest.java
@@ -29,8 +29,8 @@ public class TC47_SignInFromRewardsProgramInfoTest extends DriverTest {
 	//String USER_EMAIL = ExcelUtility.getData("TC01", "Username");
 	//String PASSWORD = ExcelUtility.getData("TC01", "SignInPassword");
 	
-	String USER_EMAIL="prod@mail.com";	
-	String PASSWORD="Test1234";	
+	String USER_EMAIL="srinivas.deva@kony.com";	
+	String PASSWORD="srinivas1#";	
 
 	@BeforeClass
 	public void setUpPage() {
